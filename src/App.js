fb23c16e17780e060b4ecff4507d667d14b65a0d
harut2003 <harut.terteryan2003@gmail.com>
2021-11-13 15:09:07 +0400

    added edit and delete functions

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import "./App.css";
-import { Greeting } from "./Greeting";
-import { Bye } from "./Greeting";
-import Product from "./Product"
+// import { Greeting } from "./Greeting";
+// import { Bye } from "./Greeting";
+// import Product from "./Product";
+import Input from "./Input";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Greeting surname="Smith" />
-        <Bye />
-        <table className="products">
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Description</th>
-          </tr>
-          <Product name="banabas" price="1$" description="Fresh bananas from Ecuador" />
-          <Product name="apple" price="2$" description="Apples from dsf" />
-        </table>
-
-
-
-      </header>
-    </div>
+    // {/* <Greeting surname="Smith" />
+    // <Bye /> */}
+    // {/* <table className="products">
+    //   <thead>
+    //     <tr>
+    //       <th>Name</th>
+    //       <th>Price</th>
+    //       <th>Description</th>
+    //     </tr>
+    //     <Product
+    //       name="banabas"
+    //       price="1$"
+    //       description="Fresh bananas from Ecuador" />
+    //     <Product
+    //       name="apple"
+    //       price="2$"
+    //       description="Apples from dsf" />
+    //   </thead>
+    // </table> */}
+    <Input />
   );
 }
 
